a744f7d63d8f6829641ebed474fadbe5e39a8f36
pawel <pawelkuzminski8701@wp.pl>
2017-02-07 16:19:36 +0100

    add class DisplayGameBoardOnTheCommandLine

---
+++ GameOfLife/displaygameboardonthecommandline.cpp
@@ -0,0 +1,6 @@
+#include "displaygameboardonthecommandline.h"
+
+DisplayGameBoardOnTheCommandLine::DisplayGameBoardOnTheCommandLine()
+{
+
+}
---
+++ GameOfLife/displaygameboardonthecommandline.h
@@ -0,0 +1,11 @@
+#ifndef DISPLAYGAMEBOARDONTHECOMMANDLINE_H
+#define DISPLAYGAMEBOARDONTHECOMMANDLINE_H
+
+
+class DisplayGameBoardOnTheCommandLine
+{
+public:
+    DisplayGameBoardOnTheCommandLine();
+};
+
+#endif // DISPLAYGAMEBOARDONTHECOMMANDLINE_H
