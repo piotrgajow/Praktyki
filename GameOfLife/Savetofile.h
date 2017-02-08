d5ee9159f4dcd76a9eff7a3213c3d42f8a73f943
postek <lukaszpostek12@gmail.com>
2017-02-08 10:36:13 +0100

    dodanie klasy Savetofile

---
+++ GameOfLife/Savetofile.h
@@ -0,0 +1,18 @@
+#ifndef SAVETOFILE_H
+#define SAVETOFILE_H
+//#include <QFileDialog>
+#include <QTextStream>
+#include <QFile>
+
+
+class SaveTofile
+{
+public:
+    SaveTofile();
+    SaveTofile(QString,QString);
+
+    ~SaveTofile();
+
+};
+
+#endif // SAVETOFILE_H
