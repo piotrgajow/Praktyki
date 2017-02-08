480d10e90b3c9f8c7307f3a8372c48edaaec8e28
dominikakk123 <dominika.szymanska@ericsson.com>
2017-02-08 12:04:51 +0100

    addTwoFilesOfConverter

--- GameOfLife/converterboolboardtostringboard.cpp
+++ GameOfLife/converterboolboardtostringboard.cpp
@@ -1,6 +1,24 @@
 #include "converterboolboardtostringboard.h"
 
-ConverterBoolBoardToStringBoard::ConverterBoolBoardToStringBoard()
-{
 
+std::vector<std::vector<char> > ConverterBoolBoardToStringBoard::converterBoolBoardToCharBoard(std::vector<std::vector<bool> > theBoard)
+{
+    for(std::size_t numberOfRow=0; numberOfRow<theBoard.size(); numberOfRow++)
+    {
+        std::vector<char>temporaryVectorToCreateStringBoard;
+        for(std::size_t numberOfColumn=0; numberOfColumn<theBoard[numberOfRow].size(); numberOfColumn++)
+        {
+            if(theBoard[numberOfRow][numberOfColumn]== 0)
+            {
+                temporaryVectorToCreateStringBoard.push_back('.');
+            }
+            else
+            {
+                temporaryVectorToCreateStringBoard.push_back('x');
+            }
+            charBoard.push_back(temporaryVectorToCreateStringBoard);
+        }
+    }
+    return charBoard;
 }
+
--- GameOfLife/converterboolboardtostringboard.h
+++ GameOfLife/converterboolboardtostringboard.h
@@ -1,11 +1,20 @@
 #ifndef CONVERTERBOOLBOARDTOSTRINGBOARD_H
 #define CONVERTERBOOLBOARDTOSTRINGBOARD_H
 
+#include <vector>
+#include <iostream>
+
+
+
 
 class ConverterBoolBoardToStringBoard
 {
 public:
-    ConverterBoolBoardToStringBoard();
+
+    std::vector<std::vector<char>> converterBoolBoardToCharBoard(std::vector<std::vector<bool>> theBoard);
+ private:
+    std::vector<std::vector<bool>> theBoard;
+    std::vector<std::vector<char>> charBoard;
 };
 
 #endif // CONVERTERBOOLBOARDTOSTRINGBOARD_H
