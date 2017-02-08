eb09ff87b686b8d9968e1a71a440b15a8dae4aa9
KamilaStefaniak <kamila.stefaniak@ericsson.com>
2017-02-08 16:24:51 +0100

    Add tester catch

--- GameOfLife/filler.h
+++ GameOfLife/filler.h
@@ -1,11 +1,16 @@
 #ifndef FILLER_H
 #define FILLER_H
-
-
+#include <vector>
+//#include "board.h"
+//#include "fillerwithrandomvalues.h"
 class Filler
 {
 public:
-    Filler();
+ Filler();
+ std::vector<std::vector<bool>> fillBoardWithRandomBoolValues( std::vector<std::vector<bool>> theBoard);
+ std::vector<std::vector<bool>> fillWithDeadCells( std::vector<std::vector<bool>> DeadBoard);
+
+
 };
 
 #endif // FILLER_H
