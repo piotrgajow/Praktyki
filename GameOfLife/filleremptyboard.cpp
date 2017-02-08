a5aafd33b1b5139820b532fc11444278e594c877
KamilaStefaniak <kamila.stefaniak@ericsson.com>
2017-02-08 11:45:32 +0100

    Create method in class FillerRandomkValues and class FillerEmptyBoard

---
+++ GameOfLife/filleremptyboard.cpp
@@ -0,0 +1,6 @@
+#include "filleremptyboard.h"
+
+FillerEmptyBoard::FillerEmptyBoard()
+{
+
+}
---
+++ GameOfLife/filleremptyboard.h
@@ -0,0 +1,11 @@
+#ifndef FILLEREMPTYBOARD_H
+#define FILLEREMPTYBOARD_H
+
+
+class FillerEmptyBoard
+{
+public:
+    FillerEmptyBoard();
+};
+
+#endif // FILLEREMPTYBOARD_H
