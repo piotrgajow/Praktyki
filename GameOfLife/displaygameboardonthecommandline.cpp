bc14829642e34a83b80c4a3aa60900153769357a
pawel <pawelkuzminski8701@wp.pl>
2017-02-09 15:36:01 +0100

    remove constructor

--- GameOfLife/displaygameboardonthecommandline.cpp
+++ GameOfLife/displaygameboardonthecommandline.cpp
@@ -1,10 +1,5 @@
 #include "displaygameboardonthecommandline.h"
 
-DisplayGameBoardOnTheCommandLine::DisplayGameBoardOnTheCommandLine()
-{
-
-}
-
 void displayGameBoardOnTheCommandLine(std::string stringConvertedFromBoard)
 {
         std::cout<<stringConvertedFromBoard;
