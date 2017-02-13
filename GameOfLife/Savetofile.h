c8a9b2035cdba9d865f538a9518d38520cd91efb
postek <lukaszpostek12@gmail.com>
2017-02-13 11:26:28 +0100

    najnowsza wersja/poniedzialek 11:20

---
+++ GameOfLife/Savetofile.h
@@ -0,0 +1,16 @@
+#ifndef SAVETOFILE_H
+#define SAVETOFILE_H
+//#include <QFileDialog>
+#include <QTextStream>
+#include <QFile>
+
+
+class SaveTofile
+{
+public:
+    SaveTofile();
+    SaveTofile(QString,QString);
+
+};
+
+#endif // SAVETOFILE_H
