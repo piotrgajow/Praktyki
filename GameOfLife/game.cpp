656ce2a31329527afbb816a711eb657b1ec78eed
KamilaStefaniak <kamila.stefaniak@ericsson.com>
2017-02-15 16:01:25 +0100

    Update game class

--- GameOfLife/game.cpp
+++ GameOfLife/game.cpp
@@ -1,28 +1,27 @@
 #include "Game.h"
 
+
 Game::Game()
 {
-Board board;
-CheckButtonPressed newPressed;
-FillerWithRandomValues filler;
-DisplayGameBoardOnTheCommandLine display;
-ConverterBoolBoardToString converter;
-Generator generator;
 filler.fillBoard(board);
+startInfiniteLoop();
+}
 
-while(1)
-    {
-    newPressed.checkStatusOfButtonPressed();
-    do
-    {
-        system("cls");
-        converter.convertBoolBoardToString((board.getTheBoard()));
-        display.displayGameBoardOnTheCommandLine(converter.getBoardConvertToString());
-        board = generator.generateNextBoard(board);
+void Game::startInfiniteLoop()
+{
+    while(1)
+        {
         newPressed.checkStatusOfButtonPressed();
-        Sleep(1000);
-    }
-    while (newPressed.getStatusOfIncrementLoop() == true);
-    system("pause");
-    }
+        do
+        {
+            system("cls");
+            converter.convertBoolBoardToString((board.getTheBoard()));
+            display.displayGameBoardOnTheCommandLine(converter.getBoardConvertToString());
+            board = generator.generateNextBoard(board);
+            newPressed.checkStatusOfButtonPressed();
+            Sleep(1000);
+        }
+        while (newPressed.getStatusOfIncrementLoop() == true);
+        system("pause");
+        }
 }
