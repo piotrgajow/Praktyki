a833504609be4a8e62fcedf0ad54b5b74285dfd5
Maciej Wieckowski <maciej.wieckowski@ericsson.com>
2017-02-16 10:39:06 +0100

    adding board constructor2

---
+++ GameOfLife/board.cpp
@@ -0,0 +1,12 @@
+#include "board.h"
+
+Board::Board()
+{
+
+}
+
+Board::Board(int numberOfRows, int numberOfColumns)
+{
+    this->theBoard.resize(numberOfColumns, std::vector<bool>(numberOfRows));
+
+}
