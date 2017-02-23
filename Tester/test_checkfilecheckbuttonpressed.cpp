dae5b162c5fa0d2ae9a11c638ecf50a1391c703e
justus88 <justusque@gmail.com>
2017-02-23 16:26:30 +0100

    update test file

--- Tester/test_checkfilecheckbuttonpressed.cpp
+++ Tester/test_checkfilecheckbuttonpressed.cpp
@@ -2,91 +2,59 @@
 #include "mock_checkbuttonpressed.h"
 
 
-TEST_CASE( "Check if SPACE changes statusOfIncrementLoop", "[checkButtonPressed]" ) {
+TEST_CASE( "Check if SPACE changes statusOfIncrementLoop", "[checkButtonPressed]" )
+{
+    CheckButtonPressed pressTest;
 
-CheckButtonPressed pressTest;
+    keybd_event(0x20, 0, 0, 0);
+    keybd_event(0x20, 0, KEYEVENTF_KEYUP, 0);
+    pressTest.checkStatusOfButtonPressed();
 
-keybd_event(0x20, 0, 0, 0);
-keybd_event(0x20, 0, KEYEVENTF_KEYUP, 0);
-pressTest.checkStatusOfButtonPressed();
+    CHECK(pressTest.getStatusOfLoop() == true);
 
-CHECK(pressTest.getStatusOfLoop() == true);
+    keybd_event(0x20, 0, 0, 0);
+    keybd_event(0x20, 0, KEYEVENTF_KEYUP, 0);
+    pressTest.checkStatusOfButtonPressed();
 
-keybd_event(0x20, 0, 0, 0);
-keybd_event(0x20, 0, KEYEVENTF_KEYUP, 0);
-pressTest.checkStatusOfButtonPressed();
-
-CHECK(pressTest.getStatusOfLoop() == false);
+    CHECK(pressTest.getStatusOfLoop() == false);
 }
 
-TEST_CASE( "check button pressed ESC OFF", "[checkButtonPressed]" ) {
+TEST_CASE( "check button pressed ESC OFF", "[checkButtonPressed]" )
+{
+    Mock_checkbuttonpressed pressTest;
 
-Mock_checkbuttonpressed pressTest;
-
-CHECK(pressTest.exit == false);
-
+    CHECK(pressTest.exit == false);
 }
 
-TEST_CASE( "check button pressed ESC ON", "[checkButtonPressed]" ) {
+TEST_CASE( "check button pressed ESC ON", "[checkButtonPressed]" )
+{
+    Mock_checkbuttonpressed pressTest;
 
-Mock_checkbuttonpressed pressTest;
+    keybd_event(0x1b, 0, 0, 0);
+    keybd_event(0x1b, 0, KEYEVENTF_KEYUP, 0);
+    pressTest.checkStatusOfButtonPressed();
 
-keybd_event(0x1b, 0, 0, 0);
-keybd_event(0x1b, 0, KEYEVENTF_KEYUP, 0);
-pressTest.checkStatusOfButtonPressed();
-
-CHECK(pressTest.exit == true);
-
+    CHECK(pressTest.exit == true);
 }
 
-TEST_CASE( "check button pressed ADD", "[checkButtonPressed]" ) {
+TEST_CASE( "Check if PLUS('+') changes value", "[checkButtonPressed]" )
+{
+    CheckButtonPressed pressTest;
 
-CheckButtonPressed pressTest;
+    keybd_event(VK_ADD, 0, KEYEVENTF_EXTENDEDKEY, 0);
+    keybd_event(VK_ADD, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+    pressTest.checkStatusOfButtonPressed();
 
-keybd_event(VK_ADD, 0, KEYEVENTF_EXTENDEDKEY, 0);
-keybd_event(VK_ADD, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
-pressTest.checkStatusOfButtonPressed();
-
-CHECK(pressTest.getHowManySec() == 2);
-
-
-keybd_event(VK_ADD, 0, KEYEVENTF_EXTENDEDKEY, 0);
-keybd_event(VK_ADD, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
-pressTest.checkStatusOfButtonPressed();
-
-CHECK(pressTest.getHowManySec() == 4);
-
-
-keybd_event(VK_ADD, 0, KEYEVENTF_EXTENDEDKEY, 0);
-keybd_event(VK_ADD, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
-pressTest.checkStatusOfButtonPressed();
-
-CHECK(pressTest.getHowManySec() == 8);
-
+    CHECK(pressTest.getHowManySecondToGenerateNextBoard() == 2);
 }
 
-TEST_CASE( "check button pressed SUBTRACT", "[checkButtonPressed]" ) {
+TEST_CASE( "Check if MINUS('-') changes value", "[checkButtonPressed]" )
+{
+    CheckButtonPressed pressTest;
 
-CheckButtonPressed pressTest;
+    keybd_event(VK_SUBTRACT, 0, KEYEVENTF_EXTENDEDKEY, 0);
+    keybd_event(VK_SUBTRACT, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+    pressTest.checkStatusOfButtonPressed();
 
-keybd_event(VK_SUBTRACT, 0, KEYEVENTF_EXTENDEDKEY, 0);
-keybd_event(VK_SUBTRACT, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
-pressTest.checkStatusOfButtonPressed();
-
-CHECK(pressTest.getHowManySec() == 0.5);
-
-
-keybd_event(VK_SUBTRACT, 0, KEYEVENTF_EXTENDEDKEY, 0);
-keybd_event(VK_SUBTRACT, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
-pressTest.checkStatusOfButtonPressed();
-
-CHECK(pressTest.getHowManySec() == 0.25);
-
-
-keybd_event(VK_SUBTRACT, 0, KEYEVENTF_EXTENDEDKEY, 0);
-keybd_event(VK_SUBTRACT, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
-pressTest.checkStatusOfButtonPressed();
-
-CHECK(pressTest.getHowManySec() == 0.125);
-
+    CHECK(pressTest.getHowManySecondToGenerateNextBoard() == 0.5);
 }
