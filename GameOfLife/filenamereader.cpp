14adcde11500eeaefe53dc262e65d00202fb5abe
justus88 <justusque@gmail.com>
2017-02-23 13:49:34 +0100

    add class filenamereader

--- GameOfLife/filenamereader.cpp
+++ GameOfLife/filenamereader.cpp
@@ -1,6 +1,18 @@
 #include "filenamereader.h"
 
+
+std::string FilenameReader::getFileName() const
+{
+    return fileName;
+}
+
 FilenameReader::FilenameReader()
 {
 
+}
+
+void FilenameReader::askUserAboutFileName()
+{
+    std::cout<<"Please input a name of file: ";
+    std::cin>>fileName;
 }
