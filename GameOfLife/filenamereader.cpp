b34e1265620818757f4ed0a6c3e8357abc6c95b1
justus88 <justusque@gmail.com>
2017-02-23 13:36:42 +0100

    add new class fileNameReader, del fileEmptBoard, rename systemcommand

---
+++ GameOfLife/filenamereader.cpp
@@ -0,0 +1,6 @@
+#include "filenamereader.h"
+
+FilenameReader::FilenameReader()
+{
+
+}
---
+++ GameOfLife/filenamereader.h
@@ -0,0 +1,11 @@
+#ifndef FILENAMEREADER_H
+#define FILENAMEREADER_H
+
+
+class FilenameReader
+{
+public:
+    FilenameReader();
+};
+
+#endif // FILENAMEREADER_H
