2418f0e0cfa24fa9431104c1b7c094d2eccdb338
zofia fratczak <zofia.fratczak@ericsson.com>
2017-02-28 11:31:02 +0100

    Use new metod getNumberOf Columns and Rows
    Conversion getBoard method to getNumberOfColumns and getNumberOfRows
    methods to take the board size.

--- Tester/test_fillerwithrandomvalues.cpp
+++ Tester/test_fillerwithrandomvalues.cpp
@@ -7,38 +7,36 @@
 TEST_CASE( "Check if top border of frame is filled with zero", "[FillerWithRandomValues]" ) {
 
 FillerWithRandomValues fillerWithRandomValues;
-Board board(10,10);
+Board board(20,20);
 fillerWithRandomValues.fillBoard(board);
 
-for(unsigned positionColumn =0; positionColumn < board.getTheBoard()[0].size();positionColumn++)
+for(unsigned  positionColumn =0; positionColumn < board.getNumberOfColumns(); positionColumn++)
 {
     CHECK(board.getTheBoard()[0][positionColumn] == 0);
 }
 }
 
-
 TEST_CASE( "Check if left border of frame is filled with zero", "[FillerWithRandomValues]" ) {
 
 FillerWithRandomValues fillerWithRandomValues;
-Board board(10,10);
+Board board(20,20);
 fillerWithRandomValues.fillBoard(board);
 
-for(unsigned positionLine = 0 ; positionLine < board.getTheBoard().size() ; positionLine++)
+for(unsigned positionLine=0 ; positionLine < board.getNumberOfRows() ; positionLine++)
 {
     CHECK(board.getTheBoard()[positionLine][0] == 0);
 }
 }
 
-
 TEST_CASE( "Check if bottom border of frame is filled with zero", "[FillerWithRandomValues]" ) {
 
 FillerWithRandomValues fillerWithRandomValues;
-Board board(10,10);
+Board board(10,20);
 fillerWithRandomValues.fillBoard(board);
 
-for(unsigned positionColumn =0; positionColumn < board.getTheBoard()[0].size();positionColumn++)
+for(unsigned positionColumn = 0; positionColumn < board.getNumberOfColumns(); positionColumn++)
 {
-    CHECK(board.getTheBoard()[board.getTheBoard().size()-1][positionColumn] == 0);
+    CHECK(board.getTheBoard()[board.getNumberOfRows()-1][positionColumn] == 0);
 }
 }
 
@@ -46,25 +44,25 @@ for(unsigned positionColumn =0; positionColumn < board.getTheBoard()[0].size();p
 TEST_CASE( "Check if right border of frame is filled with zero", "[FillerWithRandomValues]" ) {
 
 FillerWithRandomValues fillerWithRandomValues;
-Board board(10,10);
+Board board(10,20);
 fillerWithRandomValues.fillBoard(board);
 
-for(unsigned positionLine = 0 ; positionLine < board.getTheBoard().size() ; positionLine++)
-{
-    CHECK(board.getTheBoard()[positionLine][board.getTheBoard().size()-1] == 0);
-}
+for(unsigned positionLine = 0 ; positionLine < board.getNumberOfRows(); positionLine++)
+    {
+    CHECK(board.getTheBoard()[positionLine][board.getNumberOfColumns()-1] == 0);
+    }
 }
 
 
 TEST_CASE( "Check if values in the board are no higher than 1", "[FillerWithRandomValues]" ) {
 
 FillerWithRandomValues fillerWithRandomValues;
-Board board(10,10);
+Board board(20,10);
 fillerWithRandomValues.fillBoard(board);
 
-for(unsigned positionLine = 1 ; positionLine < board.getTheBoard().size()-1 ; positionLine++)
+for(unsigned positionLine = 1 ; positionLine < board.getNumberOfRows() ; positionLine++)
 {
-    for(unsigned positionColumn = 1; positionColumn < board.getTheBoard()[positionLine].size()-1;positionColumn++)
+    for(unsigned positionColumn = 1; positionColumn < board.getNumberOfColumns();positionColumn++)
     {
         CHECK(board.getTheBoard()[positionLine][positionColumn] <= 1);
     }
