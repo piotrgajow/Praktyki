4e6a3d03500ae674f9d32d1b4337f47b329405f7
postek <lukaszpostek12@gmail.com>
2017-02-28 13:13:18 +0100

    Add new class ReaderFromTxt
    Reader has parametrical constructor which gets file name and set string.

--- GameOfLife/readerfromtxtfile.cpp
+++ GameOfLife/readerfromtxtfile.cpp
@@ -1,6 +1,9 @@
 #include "readerfromtxtfile.h"
 
-ReaderFromTxtFile::ReaderFromTxtFile()
+ReaderFromTxtFile::ReaderFromTxtFile(std::string fileName)
 {
-
+    std::ifstream file(fileName);
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    readString = buffer.str();
 }
--- GameOfLife/readerfromtxtfile.h
+++ GameOfLife/readerfromtxtfile.h
@@ -1,11 +1,15 @@
 #ifndef READERFROMTXTFILE_H
 #define READERFROMTXTFILE_H
-
+#include <string>
+#include <fstream>
+#include <sstream>
 
 class ReaderFromTxtFile
 {
 public:
-    ReaderFromTxtFile();
+    ReaderFromTxtFile(std::string);
+private:
+    std::string readString;
 };
 
 #endif // READERFROMTXTFILE_H
