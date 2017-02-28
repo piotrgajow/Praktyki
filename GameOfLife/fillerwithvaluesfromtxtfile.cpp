14b1c548070c50bea185a93a2d2db4939ff7277b
postek <lukaszpostek12@gmail.com>
2017-02-28 13:51:36 +0100

    Rename function's parameters

--- GameOfLife/fillerwithvaluesfromtxtfile.cpp
+++ GameOfLife/fillerwithvaluesfromtxtfile.cpp
@@ -1,6 +1,22 @@
 #include "fillerwithvaluesfromtxtfile.h"
 
-FillerWithValuesFromTxtFile::FillerWithValuesFromTxtFile()
-{
 
+FillerWithValuesFromTxtFile::FillerWithValuesFromTxtFile(Board & board, std::__cxx11::string stringFromTxtFile)
+{
+    std::vector<std::vector<bool>> theBoard = board.getTheBoard();
+
+    int positionInString = 0;
+    for (int row=1; row<board.getTheBoard().size()-1; row++)
+    {
+        for (int column=1; column<board.getTheBoard()[0].size()-1; column++)
+        {
+            if(stringFromTxtFile[positionInString] != '\n')
+            {
+                stringFromTxtFile[positionInString] == 'X' ? theBoard[row][column] = true : theBoard[row][column] = false;
+                positionInString++;
+            }
+            else positionInString+=2;
+        }
+    }
+    board.setStateOfBoard(theBoard);
 }
--- GameOfLife/fillerwithvaluesfromtxtfile.h
+++ GameOfLife/fillerwithvaluesfromtxtfile.h
@@ -1,11 +1,12 @@
 #ifndef FILLERWITHVALUESFROMTXTFILE_H
 #define FILLERWITHVALUESFROMTXTFILE_H
-
+#include <string>
+#include "board.h"
 
 class FillerWithValuesFromTxtFile
 {
 public:
-    FillerWithValuesFromTxtFile();
+    FillerWithValuesFromTxtFile(Board&, std::string);
 };
 
 #endif // FILLERWITHVALUESFROMTXTFILE_H
