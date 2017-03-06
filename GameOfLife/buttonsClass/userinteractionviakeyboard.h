d68631fac0c66d9218613cb118e3f89f8b15002f
justus88 <justusque@gmail.com>
2017-03-06 12:53:48 +0100

    Add virtual class

---
+++ GameOfLife/buttonsClass/userinteractionviakeyboard.h
@@ -0,0 +1,13 @@
+#ifndef USERINTERACTIONVIAKEYBOARD_H
+#define USERINTERACTIONVIAKEYBOARD_H
+#include <iostream>
+
+
+class UserInteractionViaKeyboard
+{
+public:
+    virtual void actButtonPressed() = 0;
+
+};
+
+#endif // USERINTERACTIONVIAKEYBOARD_H
