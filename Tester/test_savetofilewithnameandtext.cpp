51ba8b95b9e77a6aae6757ca329cf83d72518738
dominikakk123 <dominika.szymanska@ericsson.com>
2017-03-09 15:43:36 +0100

    Add test check if file is properly open

---
+++ Tester/test_savetofilewithnameandtext.cpp
@@ -0,0 +1,30 @@
+#include "Tester_Catch.h"
+#include "..\GameOfLife\savetofilewithnameandtext.h"
+
+
+TEST_CASE("CheckIfFileOpenProperly", "[savetofilewithnameandtext]")
+{
+    std::ofstream file;
+    std::string nameOfFile="fileName.txt";
+    std::string boardSavingToFile="000\n011\n111";
+
+    SaveToFileWithNameAndText savingFile;
+    savingFile.saveFile(boardSavingToFile, nameOfFile);
+
+    bool checkIfFileOpen;
+    file.open(nameOfFile);
+
+        if(file.is_open())
+        {
+            checkIfFileOpen=true;
+        }
+        else
+        {
+            checkIfFileOpen=false;
+        }
+        file.close();
+     CHECK(checkIfFileOpen==true);
+
+     remove("fileName.txt");
+}
+
