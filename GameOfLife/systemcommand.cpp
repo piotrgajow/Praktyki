53010efdbca01cee4bd7faf96ec249a49bd27a12
Piotr Gajowniczek <piotrgajow@gmail.com>
2017-03-10 01:00:24 +0100

    Switch Sleep to usleep

--- GameOfLife/systemcommand.cpp
+++ GameOfLife/systemcommand.cpp
@@ -19,7 +19,7 @@ void SystemCommand::waitAsLongAsDeclaredToGenerateTheNextBoard(float howManySeco
 {
     if (loopStatus)
     {
-         Sleep(howManySecondToGenerateNextBoard*1000);
+        usleep(howManySecondToGenerateNextBoard*1000);
     }
 }
 
